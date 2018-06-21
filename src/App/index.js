63c2248a8ccbf551f7723fdc9a9c1d4b8fe3a7b4
Troy <36167978+troywsmith@users.noreply.github.com>
2018-06-21 01:17:37 -0400

    added content to app component as well as style

--- src/App/index.js
+++ src/App/index.js
@@ -13,13 +13,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Calify</h1>
-        <h3>Events</h3>
-        <ul> 
-          {this.state.events.map(event => 
-          <li key={event.id}>{event.description}</li>
-          )}
-        </ul>
+       <header className="header">
+          <img src="https://cdn.freebiesupply.com/logos/large/2x/spotify-2-logo-png-transparent.png" className="logo" alt="logo" />
+          <div className="info">
+            <h1 className="title">Welcome to Calify</h1>
+            <p className="intro">A very simple calendar app</p>
+          </div>
+        </header>
+        <main>
+          <div className="sidebar">
+          </div>
+          <div className="page">
+
+          
+            <h3>Events</h3>
+            <ul className="events-list"> 
+              {this.state.events.map(event => 
+              <li key={event.id}>{event.description}</li>
+              )}
+            </ul>
+          </div>
+        </main>
+        <footer>
+        </footer>
       </div>
     );
   }
